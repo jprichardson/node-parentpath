339bb6b2e9d06b23444c12e004e3cd8817362276
JP Richardson <jprichardson@gmail.com>
2013-01-26 14:58:34 -0600

    Change API

--- test/parentpath.test.js
+++ test/parentpath.test.js
@@ -1,20 +1,15 @@
 var fs = require('fs-extra')
-  , parent = require('../lib/parentpath')
-  , path = require('path-extra')
+  , pp = require('../lib/parentpath')
   , testutil = require('testutil')
-  , batch = require('batchflow')
-  , next = require('nextflow') 
   , S = require('string')
   , exec = require('child_process').exec
+  , P = require('autoresolve')
+  , walker = require('walker')
+  , path = require('path')
 
 var TEST_DIR = ''
-  , DIRS = [];
-
-var pj = path.join;
-
-function ridx() {
-  return DIRS[Math.floor(Math.random() * DIRS.length)];
-}
+  , DIRS = []
+  , RESOURCE_DIR = P('test/resources')
 
 function removePrivate(dir) {
   if (dir === null) return null
@@ -27,69 +22,52 @@ function removePrivate(dir) {
 describe('parentpath', function(){
   beforeEach(function(done) {
     TEST_DIR = testutil.createTestDir('parentpath');
+    process.chdir(TEST_DIR)
+
+    TEST_DIR = path.join(TEST_DIR, 'test')
     TEST_DIR = removePrivate(TEST_DIR)
 
-    TEST_DIR = path.resolve(TEST_DIR);
-    //console.log(TEST_DIR)
-
-    var dirs = [];
-    dirs[0] = pj(TEST_DIR, 'potter')
-    dirs[1] = pj(dirs[0], 'pages')
-    dirs[2] = pj(dirs[1], 'animals')
-    dirs[3] = pj(dirs[2], 'reptiles')
-    dirs[4] = pj(dirs[2], 'mammals')
-    dirs[5] = pj(dirs[3], 'snakes')
-    dirs[6] = pj(dirs[4], 'humans')
-    
-    var files = [];
-    files[0] = path.join(dirs[0], 'potter.json')
-    files[1] = path.join(dirs[2], 'animals.md')
-    files[2] = path.join(dirs[5], 'reptiles.md')
-
-    DIRS = dirs;
-
-    next({
-      ERROR: function(err) {
-          done(err);
-      },
-      createTD: function() {
-           var self = this;
-          fs.mkdir(TEST_DIR, this.next);
-      },
-      createDirs: function(){
-          var self = this;
-          batch(dirs).seq().each(function(i, dir, done){ fs.mkdir(dir, done); }).end(function(){ self.next(); });        
-      },
-      createFiles: function() {
-          var self = this;
-          batch(files).par().each(function(i, file, done){ fs.writeFile(file, done); }).end(function(){ self.next(); });
-      },
-      done: function() {
-          process.chdir('/tmp')
-          /*exec('tree  ' + '/tmp', function(err, stdout, stderr) {
-              console.log(stdout);
-              process.exit();
-          });*/
-          done()
-      }
-    });
+    fs.copy(RESOURCE_DIR, TEST_DIR, function(err) {
+      walker(TEST_DIR)
+      .on('dir', function(dir) {
+        DIRS.push(dir)
+      })
+      .on('end', function() {
+        done()
+      })
+    })
   })
 
 
   it('should find the path of a parent dir', function(done){
-    process.chdir(ridx());        
-    parent.find('potter/').end(function(dir) {
+    var snakeDir = ''
+    DIRS.forEach(function(d) {
+      if (S(d).endsWith(d))
+        snakeDir = d
+    })
+    if (!snakeDir) done(new Error('Cant find snake dir.'))
+
+    process.chdir(snakeDir)
+    pp('skyblog/', function(dir) {
       dir = removePrivate(dir)
-      
+        
       EQ (dir, TEST_DIR);
-      done();
+      done()
     })
   })
 
 
   it('should find the path of a parent path', function(done) {
-    process.chdir(ridx());
-    parent.find('potter/potter.json').end(function(dir) {
+    var snakeDir = ''
+    DIRS.forEach(function(d) {
+      if (S(d).endsWith(d))
+        snakeDir = d
+    })
+    if (!snakeDir) done(new Error('Cant find snake dir.'))
+
+    process.chdir(snakeDir)
+
+    pp('skyblog/sky.json', function(dir) {
       dir = removePrivate(dir)
 
       EQ (dir, TEST_DIR);
@@ -106,17 +84,25 @@ describe('parentpath', function(){
     if (!snakeDir) done(new Error('Cant find snake dir.'))
 
     process.chdir(snakeDir)
-    parent.find('animals.md').end(function(dir) {
+    pp('animals.md', function(dir) {
       dir = removePrivate(dir)
       
-      EQ (dir, path.join(TEST_DIR, 'potter/pages/animals'))
+      EQ (dir, path.join(TEST_DIR, 'skyblog/pages/animals'))
       done();
     })
   })
 
   it('should return null if the parent path cant be found', function(done) {
-    process.chdir(ridx());
-    parent.find('potter/per.json').end(function(dir) {
+    var snakeDir = ''
+    DIRS.forEach(function(d) {
+      if (S(d).endsWith(d))
+        snakeDir = d
+    })
+    if (!snakeDir) done(new Error('Cant find snake dir.'))
+
+    process.chdir(snakeDir)
+
+    pp('skyblog/per.json', function(dir) {
       EQ (dir, null);
       done();
     })
